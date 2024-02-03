623ca52143acdef8fb72db1eda5450654cefd3e7
THINUX-Official <thinuxofficial@gmail.com>
2024-02-03 10:39:15 +0530

    Create interface base.

--- front-end/src/component/DefaultComponent/DefaultComponent.tsx
+++ front-end/src/component/DefaultComponent/DefaultComponent.tsx
@@ -1,5 +1,6 @@
 import {Component} from "react";
 import {Messenger} from "../Messenger/Messenger";
+import {CreateUser} from "../User/CreateUser/CreateUser";
 
 export class DefaultComponent extends Component {
     render() {
--- front-end/src/component/Messenger/Messenger.tsx
+++ front-end/src/component/Messenger/Messenger.tsx
@@ -1,15 +1,45 @@
-import {Component} from "react";
+import React, {Component} from "react";
+
+import {CreateUser} from "../User/CreateUser/CreateUser";
 
 export class Messenger extends Component {
     render() {
         return (
             <>
-                <div className="text-center">
-                    <h1 className="text-center">Yow</h1>
-                </div>
+                <div className="bg-yellow-300">
+                    <div className="bg-yellow-300">
+                        <h1 className="text-center">Crate User</h1>
+                    </div>
 
-                <div>
+                    <div>
+                        <form>
+                            <div>
+                                <input type="text" placeholder={"Input ID"}/>
+                            </div>
 
+                            <div>
+                                <input type="email" placeholder={"Input Email"}/>
+                            </div>
+
+                            <div>
+                                <input type="text" placeholder={"Input Name"}/>
+                            </div>
+
+                            <div>
+                                <input type="text" placeholder={"Input Contact Number"}/>
+                            </div>
+
+                            <div>
+                                <input type="text" placeholder={"Input Picture"}/>
+                            </div>
+                        </form>
+
+                        <div>
+                            <button type="button">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </>
 
